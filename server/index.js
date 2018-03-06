cacbf016473da97049a23d612f7b2a7ded0de6fb
James Reynolds <jameshreynolds@gmail.com>
2018-03-06 13:40:19 -0800

    updated version#

--- server/index.js
+++ server/index.js
@@ -6,6 +6,14 @@ let app = express();
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use('/:id', express.static(__dirname + '/../client/dist'));
+app.use('/pledge/:id', express.static(__dirname + '/../client/dist'));
+
+// app.get('/pledge/:id', function (req, res) {
+//   console.log('req.params', req.params.id);
+//   console.log('Server received a post request', req.url); 
+//   res.send('hey Chas')
+// });
 
 let port = 3000;
 
